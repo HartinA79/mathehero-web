b9b9f28ac51d7b347a528b625f1a683dc8ff48dc
HartinA79 <alexander.hartinger@web.de>
2025-05-18 08:36:27 +0200

    css update

--- MatheHero.Shared/obj/Debug/net9.0/MatheHero.Shared.AssemblyInfo.cs
+++ MatheHero.Shared/obj/Debug/net9.0/MatheHero.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatheHero.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aa0f36996123f70d33c44dcccd94083eeda2f25")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatheHero.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatheHero.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MatheHero/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MatheHero.AssemblyInfo.cs
+++ MatheHero/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MatheHero.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatheHero")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4aa0f36996123f70d33c44dcccd94083eeda2f25")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatheHero")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatheHero")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
